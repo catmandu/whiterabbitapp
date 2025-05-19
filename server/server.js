f58235d97ebdf5b28b07afb0909e1fe4be24507b
Juan Uriarte <juriartemanzano@gmail.com>
2025-05-18 18:39:28 -0600

    fixing static page assignation error

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 app.use(bodyParser.json());
-app.use(path, __dirname, 'build');
+app.use(express.static(path.join(__dirname, 'build')));
 app.use('/api/active-packages', activePackagesRoutes);
 app.use('/api/hour-packages', hourPackagesRoutes);
 
